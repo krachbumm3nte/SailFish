115a670be34f39f182c0fac549dd159679dad859
johannes <johannes.gille@gmx.de>
2019-09-22 15:11:48 +0200

    added new configuration, commit before major architecture rework

--- load_image.py
+++ load_image.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     with h5py.File(sys.argv[1], 'r') as infile:
         image = infile['data'][::scale, ::scale, ::scale]
         print(f"loading file of size: {image.shape}")
-        for k,v in infile['meta'].attrs.items():
-            print(k,v)
+        for k, v in infile['meta'].attrs.items():
+            print(k, v)
     image = sitk.GetImageFromArray(image)
     show_3D_image(image)
 
